b190387be7d6e88dd3352b072ccf6e65ffbcf312
Braulio Herrera <brau.herrerahuerta@gmail.com>
2026-06-05 23:19:10 -0400

    feat() cambios de

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
   app.enableCors({
     origin: ['http://localhost:4200', 'https://brauliodtesting.com'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
@@ -17,7 +20,8 @@ async function bootstrap() {
     })
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = configService.get<number>('PORT') || 3000;
+  await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
